b1495401a2524d4fb16fef57ef5009a99d0b1597
Nickolas D'Alimonte <ND135@myscc.ca>
2025-11-04 06:12:20 -0500

    Unit tests to inputvalidator, created validation logic for ints and doubles. Created a menuhandler file. Added junit dependencies to pom

---
+++ src/main/java/InputValidator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class InputValidator {
+    private final Scanner input;
+
+    public InputValidator(Scanner input){
+        this.input = input;
+    }
+
+    public int getValidInt(){
+
+        while(true){
+            if(this.input.hasNextInt()){
+                input.nextLine();
+                return this.input.nextInt();
+            }
+            else{
+                System.out.println("Invalid input, please enter an integer");
+                input.next();
+            }
+        }
+    }
+
+    public double getValidDouble(){
+
+        while(true){
+            if(this.input.hasNextDouble()){
+                return this.input.nextDouble();
+            }
+            else{
+                System.out.println("Invalid input, please enter a number");
+                input.next();
+            }
+        }
+    }
+}
---
+++ src/main/java/MenuHandler.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MenuHandler {
+
+    public void mainMenu(Scanner input){
+
+
+    }
+}
